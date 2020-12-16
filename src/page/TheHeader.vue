870d74710dc0fcbff77929da70dd929530441440
liukangqi <liukangqi56@hotmail.com>
2020-12-16 15:02:05 +0800

    feat: 添加路由layout

---
+++ src/page/TheHeader.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <div class="header-inner">
+      <div class="logo">
+        <img src="../assets/images/logo.png" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  height: 56px;
+  width: 100%;
+  box-shadow: 0 2px 12px 0 rgba(0, 61, 112, 0.08);
+  position: relative;
+  background-color: white;
+  .header-inner {
+    height: 100%;
+    width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
--- src/router/views/index.js
+++ src/router/views/index.js
@@ -1,9 +1,22 @@
 import demoRoutes from './demo'
+import Layout from '@/page/Layout'
 export default [
-  { path: '/', redirect: '/home' },
   {
-    path: '/home',
-    component: () => import(/* webpackChunkName: "page" */ '@/views/Home.vue')
+    path: '/',
+    component: Layout,
+    children: [
+      {
+        path: '/home',
+        component: () => import(/* webpackChunkName: "page" */ '@/views/Home.vue')
+      },
+      {
+        path: '/full',
+        component: () => import(/* webpackChunkName: "page"*/ '@/views/Home.vue'),
+        meta: {
+          fullscreen: true
+        }
+      }
+    ]
   },
   ...(process.env.NODE_ENV === 'development' ? demoRoutes : [])
 ]
