4530fb2c56b7a99bf2a92ca26d43535d97d8c81f
zhouxin <861045056@qq.com>
2020-12-24 10:41:21 +0800

    fix: 提交更改

--- vue.config.js
+++ vue.config.js
@@ -51,6 +51,22 @@ module.exports = {
     // when there are many pages, it will cause too many meaningless requests
     config.plugins.delete('prefetch')
 
+    config.module
+      .rule('svg')
+      .exclude.add(resolve('src/icons'))
+      .end()
+    config.module
+      .rule('icons')
+      .test(/\.svg$/)
+      .include.add(resolve('src/icons'))
+      .end()
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end()
+
     config.when(isProduction, (config) => {
       config
         .plugin('ScriptExtHtmlWebpackPlugin')
