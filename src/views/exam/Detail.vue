db0e353217d410fc57ab06e795ff94e48bf003cf
xiangrun <xiangrun.kong@epro.com.cn>
2020-12-29 14:32:37 +0800

    feat: 详情

--- src/views/exam/Detail.vue
+++ src/views/exam/Detail.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    11
+    <common-breadcrumb ref="breadcrumb" />
+    <el-card>
+      <div slot="header"></div>
+      <div></div>
+    </el-card>
   </div>
 </template>
 
 <script>
+import CommonBreadcrumb from '@/components/common-breadcrumb/Breadcrumb'
 export default {
+  components: {
+    CommonBreadcrumb
+  },
   data() {
     return {}
   },
