1e37b9e43345f5c4d1bd994105c099381d1298e8
zihao wang <18503040758@139.com>
2021-01-29 09:19:28 +0800

    feat: 后台课程审核

--- src/api/approvalCenter.js
+++ src/api/approvalCenter.js
@@ -13,11 +13,11 @@ export const hasApproveList = (params) => get('/api/appr/v2/appr/has/approve/lis
 /**
  * 我发起的审批列表
  * */
-export const myApproveList = (params) => get('/api/appr/v2/appr/my/approve/list', params)
+export const ccApproveList = (params) => get('/api/appr/v2/appr/my/approve/list', params)
 /**
  * 抄送我的审批列表
  * */
-export const ccApproveList = (params) => get('/api/appr/v2/appr/cc/approve/list', params)
+export const myApproveList = (params) => get('/api/appr/v2/appr/cc/approve/list', params)
 /**
  * 待我审批->详情编辑
  * @param {String} params.apprNo 
