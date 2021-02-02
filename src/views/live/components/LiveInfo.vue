becd5fc80c1b72d09fe6f6c196866bbfc54d1c23
xiangrun <xiangrun.kong@epro.com.cn>
2021-02-02 15:29:46 +0800

    fix: 观看条件。学员总数、直播验证码字段以及逻辑对接

--- src/views/live/components/LiveInfo.vue
+++ src/views/live/components/LiveInfo.vue
@@ -27,7 +27,7 @@
         学员列表
       </div>
       <common-table
-        v-if="data.qcodeImageUrl"
+        v-if="data.authType === 'direct'"
         id="demo"
         ref="table"
         :columns="columnsVisible"
@@ -41,13 +41,13 @@
       <div v-else>
         <div>
           <span>学员总数：</span>
-          <span>{{ data.qcodeImageUrl }}</span>
+          <span>{{ data.traineeCount }}</span>
         </div>
         <div>学员登录平台即可观看，链接分享给任意人员可以进行观看</div>
         <!-- 需要验证码 -->
-        <div v-if="data.qcodeImageUrl">
+        <div v-if="data.authType === 'code'">
           <span>直播验证码：</span>
-          <span>{{ data.qcodeImageUrl }}</span>
+          <span>{{ data.authSecretOrCode }}</span>
         </div>
       </div>
     </div>
