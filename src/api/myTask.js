19501e0a50c13f7e7bcb6f3aee4ba626ba18be81
Your Namewangzihao <you@example.com18503040758@139.com>
2021-03-29 20:39:16 +0800

    feat: myTask-api

--- src/api/myTask.js
+++ src/api/myTask.js
@@ -1,4 +1,4 @@
-import { post } from '@/util/axios'
+import { post, get } from '@/util/axios'
 
 /**
  * 查询我的选修课程
@@ -7,3 +7,12 @@ import { post } from '@/util/axios'
  * @param {Number} params.pageSize 每页条数
  */
 export const myElectiveCourse = (params) => post('/manage/v1/study/myElectiveCourse', params)
+
+/**
+ * 查询学习待办条数
+ */
+export const studyTodoNum = (params) => get('/manage/v1/study/studyTodoNum', params)
+/**
+ * 查询考试待办条数
+ */
+export const examTodoNum = (params) => get('/manage/v1/study/examTodoNum', params)
