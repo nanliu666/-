1758799531001618eec7b9345bbe307d2689339c
lianghuajin <1034572928@qq.com>
2021-03-29 22:10:45 +0800

    feat: 请求头更改

--- src/config/website.js
+++ src/config/website.js
@@ -2,8 +2,8 @@
  * 全局配置文件
  */
 export default {
-  clientId: 'Saber', // 客户端id
-  clientSecret: 'Saber_PC_2020ePRO', // 客户端密钥
+  clientId: 'Admin', // 客户端id
+  clientSecret: 'Admin_PC_2020ePRO', // 客户端密钥
   captchaMode: 'captcha',
   trainDataKey: 'elearning-foreground-train-detail-data'
 }
