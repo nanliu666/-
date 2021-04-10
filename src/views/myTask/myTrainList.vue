9626c1de50503a49882ed356e937316e2d50010a
feng <1612169603@qq.com>
2021-04-10 11:01:29 +0800

    fix: bug 1897 【前台我的任务】我的任务-培训，点击培训详情，培训名称未显示

--- src/views/myTask/myTrainList.vue
+++ src/views/myTask/myTrainList.vue
@@ -89,13 +89,14 @@ export default {
   },
   methods: {
     toDetail(card) {
-      const { id: trainId, trainWay, userType } = card
+      const { id: trainId, trainWay, userType, menuName: title } = card
       this.$router.push({
         path: '/train/detail',
         query: {
           trainId,
           trainWay,
-          userType
+          userType,
+          title
         }
       })
     },
